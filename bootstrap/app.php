39dd6f17ed1ee57109e2770a6b38d488571d6a22
valik-pc <valik.v1per@gmail.com>
2015-11-29 20:10:19 +0200

    some updates

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -76,8 +76,8 @@ $app->singleton(
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+ $app->register(App\Providers\AppServiceProvider::class);
+ $app->register(App\Providers\EventServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
